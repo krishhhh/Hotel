1e0b498b9864af61abb696fb037e91adcd419dd8
krishhhh <krishan111180@gmail.com>
2015-10-30 14:46:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 $('form.ajax').on('submit',function(){
 var that=$(this),
 url=that.attr('action'),
-method=that.attr('method'),
+type=that.attr('method'),
 data={};
 
 that.find('[name]').each(function(index,value){
-  console.log(value);
+  var that = $(this),
+  name = that.attr('name'),
+  value = that.val();
+  
+  data[name] = value;
+});
+
+$.ajax({
+  url: url,
+  type: type,
+  data: data,
+  sucess: function(response){
+    console.log(response);
+  }
 });
 
 return false;
